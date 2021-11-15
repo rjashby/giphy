ee9042cf4c4ceca3f88e9249153dce21b4482979
Hannah Y <hannah@corgibyte.com>
2021-11-15 13:41:39 -0800

    Implement trending and random search
    Co-authored-by: Ryan Ashby <rjashby@users.noreply.github.com>
    Co-authored-by: Frank-Proulx <Frank-Proulx@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -21,6 +21,43 @@ $("#search").submit((event) => {
 });
 
 function outputGifs(response) {
-  const imgUrl = response.data[0].images.original.url;
-  $("#outputs").html(`<img src="${imgUrl}">`);
+  let htmlString = "";
+  if (Array.isArray(response.data)) {
+    for (let i = 0; i < response.data.length; i++) {
+      const imgUrl = response.data[i].images.original.url;
+      htmlString = htmlString.concat(`<img src="${imgUrl}">`);
+    }
+  } else {
+    const imgUrl = response.data.images.original.url;
+    htmlString = `<img src=${imgUrl}">`;
+  }
+  $("#outputs").html(htmlString);
 }
+
+$("#trendSearch").click(() => {
+
+  let request = new XMLHttpRequest();
+  const url = `http://api.giphy.com/v1/gifs/trending?&api_key=${process.env.API_KEY}`;
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      outputGifs(response);
+    }
+  };
+  request.open("GET", url, true);
+  request.send();  
+});
+
+$("#randomSearch").click(() => {
+
+  let request = new XMLHttpRequest();
+  const url = `http://api.giphy.com/v1/gifs/random?&api_key=${process.env.API_KEY}`;
+  request.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      outputGifs(response);
+    }
+  };
+  request.open("GET", url, true);
+  request.send();
+});
